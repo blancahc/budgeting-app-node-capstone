7d93c8f61654a11d3442e1de1ea0487b4d1b9581
Blanca Hernandez <blamca1029@gmail.com>
2018-08-17 16:26:28 -0700

    removed unecessary commented out code from files in models.js

--- models/category.js
+++ models/category.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
 
 const categorySchema = new mongoose.Schema({
     categoryName: {
--- models/subcategory.js
+++ models/subcategory.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
 
 const subcategorySchema = new mongoose.Schema({
     subcategoryName: {
